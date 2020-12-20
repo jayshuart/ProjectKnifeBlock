55a7128a41327b8ae2a10ddfb6013f40b742af6b
Joel Shuart <joelshuart@gmail.com>
2020-12-20 15:36:20 -0500

    added splash screen (in the wrong branch bc im an asshole to myself) but its cute!

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private Button playBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClickPlay()
+    {
+        playBtn.interactable = false;
+        anim.Play("Selected");
+        StartCoroutine(gotoGame());
+    }
+
+    IEnumerator gotoGame()
+    {
+        yield return new WaitForSeconds(.45f);
+        SceneManager.LoadScene("MainGame");
+    }
+}
