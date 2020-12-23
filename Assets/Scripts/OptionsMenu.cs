a649691c60f401fb2e5061d6dc2da47943121d4e
Joel Shuart <joelshuart@gmail.com>
2020-12-22 22:31:11 -0500

    Had to use ugly workarounds, but saved selected music is preselected in options now

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class OptionsMenu : MonoBehaviour
@@ -7,25 +9,51 @@ public class OptionsMenu : MonoBehaviour
     [SerializeField] private AudioSource music;
     [SerializeField] private AudioClip intenseMusic;
     [SerializeField] private AudioClip relaxedMusic;
+    [SerializeField] private Button intenseBtn;
+    [SerializeField] private Button relaxedBtn;
+    [SerializeField] private Button muteBtn;
     // Start is called before the first frame update
     void Start()
     {
         music = SoundManager.Instance.gameObject.GetComponent<AudioSource>();
-        switch(PlayerPrefs.GetString("music"))
+        StartCoroutine(autoSelectBtn_workaround());
+        StartCoroutine(close_workaround());
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(autoSelectBtn_workaround());
+    }
+
+    IEnumerator autoSelectBtn_workaround()
+    {
+        yield return null; //yiled until end of the frame
+        EventSystem.current.SetSelectedGameObject(null);
+        switch(PlayerPrefs.GetString("music", "relaxed"))
         {
             case "intense":
+                intenseBtn.Select();
                 playIntenseMusic();
                 break;
 
             case "relaxed":
+                relaxedBtn.Select();
                 playRelaxedMusic();
                 break;
 
             case "mute":
+                muteBtn.Select();
                 muteMusic();
                 break;
         }
     }
+
+    IEnumerator close_workaround()
+    { 
+        yield return null;
+        this.closeOptions();
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -44,18 +72,24 @@ public class OptionsMenu : MonoBehaviour
     }
 
     public void playIntenseMusic(){
+        if(music.clip == intenseMusic) { return; }
+
         PlayerPrefs.SetString("music", "intense");
         music.clip = intenseMusic;
         music.Play();
     }
 
     public void playRelaxedMusic(){
+        if(music.clip == relaxedMusic) { return; }
+
         PlayerPrefs.SetString("music", "relaxed");
         music.clip = relaxedMusic;
         music.Play();
     }
 
     public void muteMusic(){
+        if(music.clip == null) { return; }
+
         PlayerPrefs.SetString("music", "mute");
         music.Stop();
         music.clip = null;
